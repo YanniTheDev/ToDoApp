efada3cf5b1996a93460e775ebebebb1375b3eea
Yanxuan Weng <yannithedev@gmail.com>
2024-03-23 20:18:17 +1300

    Created and finished COMPLETE task feature

--- ToDo App/src/Components/Tasks.tsx
+++ ToDo App/src/Components/Tasks.tsx
@@ -2,21 +2,31 @@ import "../CSS Stylings/Reusables.css";
 import "../CSS Stylings/Resets.css";
 import "../CSS Stylings/Component Stylings/Tasks.css";
 
+import { ITask } from "../App";
+
 interface ITasks {
-    taskName: string,
-    id: number,
-    deleteTaskFunc: any
+    task: ITask,
+    deleteTaskFunc: any,
+    completeTaskFunc: any
 }
 
 export const Tasks = (props:ITasks) => {
     return (
         <div className="tasks flex-c-c flex-row">
-            <h1 className="tasks-name">{props.taskName}</h1>
+            <h1 className={`tasks-name ${props.task.completed ? "completed-task-bg-color" : "uncomplete-task-bg-color"}`} >
+                {props.task.taskName}
+            </h1>
 
             <button onClick={() => {
-                props.deleteTaskFunc(props.id)
+                props.deleteTaskFunc(props.task.id)
             }}>
                 X
+            </button>
+
+            <button onClick={() => {
+                props.completeTaskFunc(props.task.id)
+            }}>
+                Complete
             </button>
         </div>
     );
